bb0e2be4fcc46595d9ff7a22e6bb1bec7a219e4d
Napoleon de Mesa <napdemesa89@gmail.com>
2019-11-26 23:55:58 -0600

    added dataframe

--- twitch_scrape.py
+++ twitch_scrape.py
@@ -8,6 +8,7 @@ Version: 1.0
 import requests
 import sys
 import json
+import pandas as pd
 
 def _envget():
     file_contents = []
@@ -23,29 +24,44 @@ def _envget():
     return credentials
 
 def main():
+    overwatch_raw_data = {'user_name': [], 'user_id': [], 'broadcast_id': [], 'stream_start_time': [], 'viewer_count': []}
     credentials = _envget()
     try:
-        s = requests.Session()
-        s.headers.update({'Client-ID': credentials['TWITCH_CLIENT_ID']})
+        session = requests.Session()
+        session.headers.update({'Client-ID': credentials['TWITCH_CLIENT_ID']})
     except Exception as e:
         print(e)
     try:
-        twitch_game_response = s.get('https://api.twitch.tv/helix/streams', params = {
-            'game_id': 488552,
-            'first':  100
-        })
+        top_overwatch_streams_first_page = fetch_top_overwatch_streams(session)
+        populate_dictionary(top_overwatch_streams_first_page[0], overwatch_raw_data)
+        top_overwatch_streams_second_page = fetch_top_overwatch_streams(session, top_overwatch_streams_first_page[1])
+        populate_dictionary(top_overwatch_streams_second_page[0], overwatch_raw_data)
+        overwatch_stream_data = pd.DataFrame.from_dict(overwatch_raw_data)
+        print(overwatch_stream_data)
     except Exception as e:
         print(e)
 
+def populate_dictionary(overwatch_streams, overwatch_raw_data):
+    for stream in overwatch_streams:
+        overwatch_raw_data['user_name'].append(stream['user_name'])
+        overwatch_raw_data['user_id'].append(stream['user_id'])
+        overwatch_raw_data['broadcast_id'].append(stream['id'])
+        overwatch_raw_data['stream_start_time'].append(stream['started_at'])
+        overwatch_raw_data['viewer_count'].append(stream['viewer_count'])
+
+    return overwatch_raw_data
+
+def fetch_top_overwatch_streams(session, cursor=None):
+    twitch_game_response = session.get('https://api.twitch.tv/helix/streams', params = {
+        'game_id': 488552,
+        'first':  100,
+        'after': cursor
+    })
     twitch_game_response = twitch_game_response.json()
     overwatch_streams = list(twitch_game_response.values())[0]
-    for stream in overwatch_streams:
-        print('---------- New Stream ----------')
-        print('Username: ' + str(stream['user_name']))
-        print('User ID: ' + str(stream['user_id']))
-        print('Broadcast ID: ' + str(stream['id']))
-        print('Started streaming at: ' + str(stream['started_at']))
-        print('Viewer Count: ' + str(stream['viewer_count']))
+    starting_cursor = list(twitch_game_response.values())[1]
+    
+    return overwatch_streams, starting_cursor
 
 if __name__ == '__main__':
     main()
