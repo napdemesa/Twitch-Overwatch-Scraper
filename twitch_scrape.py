71a7c4b1362d96c8573e45bcff72281a159cd3d8
Napoleon de Mesa <napoleon.demesa@nielsen.com>
2021-04-03 03:31:52 -0500

    add game_id, game_name, and all streams in game category

--- twitch_scrape.py
+++ twitch_scrape.py
@@ -25,21 +25,89 @@ def _envget():
             for line in file:
                 file_contents.append(line.strip())
             credentials['TWITCH_CLIENT_ID'] = file_contents[0]
-            credentials['DB_USER'] = file_contents[1]
-            credentials['DB_PASS'] = file_contents[2]
-            credentials['DB_HOST'] = file_contents[3]
-            credentials['DB_PORT'] = file_contents[4]
-            credentials['DB_NAME'] = file_contents[5]
+            credentials['TWITCH_CLIENT_SECRET'] = file_contents[1]
+            credentials['DB_USER'] = file_contents[2]
+            credentials['DB_PASS'] = file_contents[3]
+            credentials['DB_HOST'] = file_contents[4]
+            credentials['DB_PORT'] = file_contents[5]
+            credentials['DB_NAME'] = file_contents[6]
     except Exception as e:
         print(e)
 
     return credentials
+
+
+def check_auth(credentials, session):
+        twitch_game_response = session.post('https://id.twitch.tv/oauth2/token', params = {
+            'client_id': credentials['TWITCH_CLIENT_ID'],
+            'client_secret': credentials['TWITCH_CLIENT_SECRET'],
+            'grant_type': 'client_credentials'
+        })
+        twitch_game_response = twitch_game_response.json()
+        access_token = twitch_game_response['access_token']
+
+        return access_token
+
+
+def populate_dictionary(streams, stream_raw_data):
+    for stream in streams:
+        stream_start_time = str(stream['started_at'][:10]) + ' ' + str(stream['started_at'][11:19])
+        stream_raw_data['id'].append(str(stream['id'] + '-' + CLEAN_TIME))
+        stream_raw_data['user_id'].append(stream['user_id'])
+        stream_raw_data['user_name'].append(stream['user_name'])
+        stream_raw_data['game_id'].append(stream['game_id'])
+        stream_raw_data['game_name'].append(stream['game_name'])
+        stream_raw_data['broadcast_id'].append(stream['id'])
+        stream_raw_data['stream_title'].append(stream['title'])
+        stream_raw_data['viewer_count'].append(stream['viewer_count'])
+        stream_raw_data['stream_start_time'].append(stream_start_time)
+        stream_raw_data['pull_time'].append(CLEAN_TIME)
+
+    return stream_raw_data
+
+def fetch_top_overwatch_streams(credentials, session, access_token, cursor=None):
+    # Overwatch: 488552, Apex: 511224
+    twitch_stream_response = session.get('https://api.twitch.tv/helix/streams', 
+                                        headers = {
+                                            'Authorization': 'Bearer ' + access_token, 
+                                            'Client-ID': credentials['TWITCH_CLIENT_ID']}, 
+                                        params = {
+                                            'game_id': 488552,
+                                            'first':  100,
+                                            'after': cursor
+                                        })
+    twitch_stream_response = twitch_stream_response.json()
+    streams = twitch_stream_response['data']
+    if 'pagination' not in twitch_stream_response:
+        return streams, None
+    cursor = twitch_stream_response['pagination']['cursor']
+
+    return streams, cursor
+
+
+def clear_data(stream_raw_data):
+    stream_raw_data['id'].clear()
+    stream_raw_data['user_id'].clear()
+    stream_raw_data['user_name'].clear()
+    stream_raw_data['game_id'].clear()
+    stream_raw_data['game_name'].clear()
+    stream_raw_data['broadcast_id'].clear()
+    stream_raw_data['stream_title'].clear()
+    stream_raw_data['viewer_count'].clear()
+    stream_raw_data['stream_start_time'].clear()
+    stream_raw_data['pull_time'].clear()
+
+    return stream_raw_data
+
+
 def write_overwatch_data(credentials, overwatch_stream_data):
-    table_name = 'overwatch_data_{}_{}'.format(YEAR, MONTH)
+    table_name = f'overwatch_data_{YEAR}_{MONTH}'
     dtypes = {
         'id': VARCHAR(255),
-        'user_name': VARCHAR(255),
         'user_id': BIGINT(),
+        'user_name': VARCHAR(255),
+        'game_id': BIGINT(),
+        'game_name': VARCHAR(255),
         'broadcast_id': BIGINT(),
         'stream_title': VARCHAR(511),
         'viewer_count': BIGINT(),
@@ -47,69 +115,53 @@ def write_overwatch_data(credentials, overwatch_stream_data):
         'pull_time': DateTime()
     }
     try:
-        engine = create_engine('mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(credentials['DB_USER'], credentials['DB_PASS'], credentials['DB_HOST'], credentials['DB_PORT'], credentials['DB_NAME']))
-        overwatch_stream_data.to_sql(table_name, con = engine, if_exists = 'append', index = False, dtype = dtypes)
+        engine = create_engine('mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(credentials['DB_USER'], 
+                                                                              credentials['DB_PASS'], 
+                                                                              credentials['DB_HOST'], 
+                                                                              credentials['DB_PORT'], 
+                                                                              credentials['DB_NAME']))
+        overwatch_stream_data.to_sql(table_name, 
+                                     con = engine, 
+                                     if_exists = 'append', 
+                                     index = False, 
+                                     dtype = dtypes)
         add_primary_key(engine, table_name)
-        print('Added primary key to table.')
     except Exception as e:
         print(e)
 
 def add_primary_key(engine, table_name):
     try:
         with engine.connect() as con:
-            con.execute('ALTER TABLE `{}` ADD PRIMARY KEY(`id`);'.format(table_name))
+            con.execute(f'ALTER TABLE `{table_name}` ADD PRIMARY KEY(`id`);')
     except Exception as e:
         print(e)
-    
+
 
 def main():
-    overwatch_raw_data = {'id':[], 'user_name': [], 'user_id': [], 'broadcast_id': [], 'stream_title': [], 'viewer_count': [], 'stream_start_time': [], 'pull_time': []}
     credentials = _envget()
+    session = requests.Session()
+    stream_raw_data = {'id':[],
+                       'user_id': [], 
+                       'user_name': [], 
+                       'game_id': [], 
+                       'game_name': [],
+                       'broadcast_id': [], 
+                       'stream_title': [], 
+                       'viewer_count': [], 
+                       'stream_start_time': [], 
+                       'pull_time': []}
     try:
-        session = requests.Session()
-        session.headers.update({'Client-ID': credentials['TWITCH_CLIENT_ID']})
+        access_token = check_auth(credentials, session)
+        cursor = ''
+        while cursor is not None:
+            streams, cursor = fetch_top_overwatch_streams(credentials, session, access_token, cursor)
+            data = populate_dictionary(streams, stream_raw_data)
+            stream_data = pd.DataFrame.from_dict(data)
+            write_overwatch_data(credentials, stream_data)
+            stream_raw_data = clear_data(data)
     except Exception as e:
         print(e)
-    
-    try:
-        print('Fetching first page of the top Overwatch streams...')
-        top_overwatch_streams_first_page = fetch_top_overwatch_streams(session)
-        populate_dictionary(top_overwatch_streams_first_page[0], overwatch_raw_data)
-        print('Fetching second page of the top Overwatch streams...')
-        top_overwatch_streams_second_page = fetch_top_overwatch_streams(session, top_overwatch_streams_first_page[1])
-        populate_dictionary(top_overwatch_streams_second_page[0], overwatch_raw_data)
-        overwatch_stream_data = pd.DataFrame.from_dict(overwatch_raw_data)
-        print('Writting to database...')
-        write_overwatch_data(credentials, overwatch_stream_data)
-        print('success!')
-    except Exception as e:
-        print(e)
 
-def populate_dictionary(overwatch_streams, overwatch_raw_data):
-    for stream in overwatch_streams:
-        stream_start_time = str(stream['started_at'][:10]) + ' ' + str(stream['started_at'][11:19])
-        overwatch_raw_data['id'].append(str(stream['id'] + '-' + CLEAN_TIME))
-        overwatch_raw_data['user_name'].append(stream['user_name'])
-        overwatch_raw_data['user_id'].append(stream['user_id'])
-        overwatch_raw_data['broadcast_id'].append(stream['id'])
-        overwatch_raw_data['stream_title'].append(stream['title'])
-        overwatch_raw_data['viewer_count'].append(stream['viewer_count'])
-        overwatch_raw_data['stream_start_time'].append(stream_start_time)
-        overwatch_raw_data['pull_time'].append(CLEAN_TIME)
-
-    return overwatch_raw_data
-
-def fetch_top_overwatch_streams(session, cursor=None):
-    twitch_game_response = session.get('https://api.twitch.tv/helix/streams', params = {
-        'game_id': 488552,
-        'first':  100,
-        'after': cursor
-    })
-    twitch_game_response = twitch_game_response.json()
-    overwatch_streams = list(twitch_game_response.values())[0]
-    starting_cursor = list(twitch_game_response.values())[1]
-
-    return overwatch_streams, starting_cursor['cursor']
 
 if __name__ == '__main__':
     main()
